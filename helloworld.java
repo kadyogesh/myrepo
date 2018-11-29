cd7deefe7965a3f43cdeda1e4aa99c3c43ab068a
kadyogesh <yogesh.kad29@gmail.com>
2018-11-29 16:44:03 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,9 @@
-public class App 
+class HelloWorld
 {
-    public static void main( String[] args )
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String args[])
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello, World");
     }
 }
